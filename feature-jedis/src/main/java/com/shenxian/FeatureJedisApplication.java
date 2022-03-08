f00aa380dc065e050b5799255e51aacd3bc4457a
tangshenxian <1125584876@qq.com>
2022-03-08 10:26:04 +0800

    jedis分布式锁模拟抢单

---
+++ feature-jedis/src/main/java/com/shenxian/FeatureJedisApplication.java
@@ -0,0 +1,16 @@
+package com.shenxian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: shenxian
+ * @date: 2022/3/8 9:22
+ */
+@SpringBootApplication
+public class FeatureJedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeatureJedisApplication.class, args);
+    }
+}
