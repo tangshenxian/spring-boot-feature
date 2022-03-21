0b8759b363c5f134d9f994c497e17775ddc340f0
shenxian <1125584876@qq.com>
2022-03-21 16:29:29 +0800

    druid数据眼整合

---
+++ feature-druid/src/main/java/com/shenxian/FeatureDruidApplication.java
@@ -0,0 +1,17 @@
+package com.shenxian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: shenxian
+ * @date: 2022/3/21 15:38
+ */
+@SpringBootApplication
+public class FeatureDruidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeatureDruidApplication.class, args);
+    }
+
+}
